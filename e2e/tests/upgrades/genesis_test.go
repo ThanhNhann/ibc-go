e190ff73066f5cd336084101cd417f5550924a8b
ducnt87 <ntd1312000@gmail.com>
2023-05-12 11:06:24 +0700

    re-config

--- e2e/tests/upgrades/genesis_test.go
+++ e2e/tests/upgrades/genesis_test.go
@@ -3,7 +3,6 @@ package upgrades
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"os"
 	"testing"
 	"time"
@@ -136,18 +135,9 @@ func (s *GenesisTestSuite) HaltChainAndExportGenesis(ctx context.Context, chain 
 	err = chain.StopAllNodes(ctx)
 	s.Require().NoError(err, "error stopping node(s)")
 
-	configFileOverrides := make(map[string]any)
 	appTomlOverrides := make(test.Toml)
 	appTomlOverrides["halt-height"] = 0
-	configFileOverrides["config/app.toml"] = appTomlOverrides
-	chainOpts := func(options *testconfig.ChainOptions) {
-		options.ChainAConfig.ConfigFileOverrides = configFileOverrides
-	}
-
-	// create chains with specified chain configuration options
-	chain, chainb := s.GetChains(chainOpts)
-	fmt.Println(chainb)
-
+	chain.Config().ConfigFileOverrides["config/app.toml"] = appTomlOverrides
 	err = chain.StartAllNodes(ctx)
 	s.Require().NoError(err, "error starting node(s)")
 
