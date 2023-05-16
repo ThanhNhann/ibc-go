83673f57370fd3f1b56776cf64297d7d5f5def7a
ducnt87 <ntd1312000@gmail.com>
2023-05-16 14:50:31 +0700

    add bash

--- e2e/dockerutil/dockerutil.go
+++ e2e/dockerutil/dockerutil.go
@@ -83,8 +83,11 @@ func SetGenesisContentsToContainer(t *testing.T, ctx context.Context, chain *cos
 }
 
 func ReconfigureHaltHeight(t *testing.T, ctx context.Context, chain *cosmos.CosmosChain) error {
-	cmd := `sed -i "s/halt-height = .*/halt-height = "0"/"`
-	_, _, err := chain.Validators[0].Exec(ctx, []string{cmd, fmt.Sprintf("/var/cosmos-chain/%s/config/app.toml", chain.Config().Name)}, nil)
+	err := chain.Validators[0].CopyFile(ctx, "./scripts/reconfig.sh", "/")
+	if err != nil {
+		return err
+	}
+	_, _, err = chain.Validators[0].Exec(ctx, []string{"bash reconfig.sh"}, nil)
 	if err != nil {
 		return err
 	}
---
+++ e2e/scripts/reconfig.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sed -i "s/halt-height = .*/halt-height = "0"/" /var/cosmos-chain/simapp-a/config/app.toml
